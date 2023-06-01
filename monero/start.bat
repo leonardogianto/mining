f3b3368f38eb6ca7ca01ba6bfe38a4a27b1bdba7
Leonardo Gianto <lgianto16@gmail.com>
2023-06-02 01:01:33 +0700

    added monero

---
+++ monero/start.bat
@@ -0,0 +1,3 @@
+cd /d "%~dp0"
+xmrig.exe -a rx/0 -o sg.monero.herominers.com:1111 -u 43bzrZwcmQxdB5f5BPEt64fK5bhr5FfwuWqFvBQjbDqNTQNpsLvftonc4k4tUTYs53P8qg6qNHyS6CXmDrfneemjAviN3hQ -k --tls -p leo_work001
+pause
